c1911d30d6e84155d419264cca8309bf4c63478e
Mike Ferrete <m.ferrete88@gmail.com>
2022-05-30 13:51:38 -0800

    added model and router files for favoriteRecipes

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 // Declare routers here
+const FavoriteRecipesRouter = require('./favoriteRecipes/favoriteRecipes-router');
 
 const server = express();
 
 server.use(express.json());
 // Use routers here
+server.use('/api/favoriteRecipes', FavoriteRecipesRouter)
 
 module.exports = server;
