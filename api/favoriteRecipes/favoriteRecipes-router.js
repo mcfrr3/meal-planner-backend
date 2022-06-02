706b7ed5af61b036194d221cc85f83ae3e4013b7
Mike Ferrete <m.ferrete88@gmail.com>
2022-06-02 11:25:47 -0800

    fixing errors on heroku - missing export in router

--- api/favoriteRecipes/favoriteRecipes-router.js
+++ api/favoriteRecipes/favoriteRecipes-router.js
@@ -3,9 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+  res.end();
 });
 
 router.post('/', (req, res) => {
-  
+  res.end();
 });
+
+module.exports = router;
