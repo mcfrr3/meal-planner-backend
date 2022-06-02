2d70014ed4c59c1fb8fd180651cadcd4a71e8353
Mike Ferrete <m.ferrete88@gmail.com>
2022-06-02 10:29:23 -0800

    changes?

--- api/favoriteRecipes/favoriteRecipes-model.js
+++ api/favoriteRecipes/favoriteRecipes-model.js
@@ -1,23 +1,23 @@
 const db = require('../../data/db-config');
 
 const favRecipesTable = 'favorite_recipe';
-const userTable = 'user';
+const userTable = 'users';
 const recipeTable = 'recipe';
 
 exports.getAll = () => {
   return db(`${recipeTable} as r`)
-    .join(`${favRecipesTable} as fr`, 'r.recipeId', 'fr.recipeId')
-    .join(`${userTable} as u`, 'fr.userId', 'u.userId');
+    .join(`${favRecipesTable} as fr`, 'r.recipe_id', 'fr.recipe_id')
+    .join(`${userTable} as u`, 'fr.user_id', 'u.user_id');
 }
 
 exports.getBy = filter => {
   return db(`${recipeTable} as r`)
-    .join(`${favRecipesTable} as fr`, 'r.recipeId', 'fr.recipeId')
-    .join(`${userTable} as u`, 'fr.userId', 'u.userId')
+    .join(`${favRecipesTable} as fr`, 'r.recipe_id', 'fr.recipe_id')
+    .join(`${userTable} as u`, 'fr.user_id', 'u.user_id')
     .where(filter);
 }
 
-exports.add = newFavoriteRecipe => {
+exports.add = async newFavoriteRecipe => {
   // WIP
   // return db(`${favRecipesTable}`).insert(newFavoriteRecipe)
 }
